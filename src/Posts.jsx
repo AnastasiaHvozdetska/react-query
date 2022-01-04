716faf4351d53b2bdc8292b2412c0cab6fe750e4
Anastasia <anastasia9297@gmail.com>
2022-01-04 17:52:33 +0200

    started

---
+++ src/Posts.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { PostDetail } from "./PostDetail";
+
+const maxPostPage = 10;
+
+async function fetchPosts() {
+  const response = await fetch(
+    "https://jsonplaceholder1.typicode.com/posts?_limit=10&_page=0"
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const { data, isError, isLoading } = useQuery("posts", fetchPosts);
+
+  console.log(data, "data");
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) return <h3>Oop, something went wrong</h3>;
+
+  return (
+    <>
+      <ul>
+        {data?.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Prev page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
